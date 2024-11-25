bfd27b6c9d9834f303f3e5e9b845a15524a9825e
JD John <ubunteen2@gmail.com>
2024-11-25 16:27:37 -0500

    Created spring boot application with spring initializr

---
+++ src/main/java/dev/austin/springproject/WelcomeMSG.java
@@ -0,0 +1,14 @@
+package dev.austin.springproject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WelcomeMSG {
+
+    public String getWelcomeMSG() {
+        return "Welcome to Spring boot project";
+
+
+    }
+
+}
