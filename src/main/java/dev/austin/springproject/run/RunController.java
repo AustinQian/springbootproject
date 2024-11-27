76f30f4d4b771c46a1c3e88c784745e1d8be0294
JD John <ubunteen2@gmail.com>
2024-11-27 16:03:41 -0500

    Added validation to post and update controls using spring boot starter validation

--- src/main/java/dev/austin/springproject/run/RunController.java
+++ src/main/java/dev/austin/springproject/run/RunController.java
@@ -1,14 +1,12 @@
 package dev.austin.springproject.run;
 
+//import jakarta.validation.Valid;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,8 +30,30 @@ public class RunController {
     Run findById(@PathVariable Integer id) {
         Optional<Run> run = runRepository.findById(id);
         if(run.isEmpty()){
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            throw new RunNotFoundException();
         }
         return run.get();
+    }
+
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    void create(@Valid @RequestBody Run run){
+        runRepository.create(run);
+    }
+
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    void update(@Valid@RequestBody Run run, @PathVariable Integer id){
+        runRepository.update(run, id);
+    }
+
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id){
+        //Controller need to match repository's input params
+        runRepository.delete(id);
     }
 }
